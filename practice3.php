598be185a9a042201a806d9bc48b160ce51b3696
kang ji soo <kangjs3779@gmail.com>
2023-09-11 16:20:15 +0900

    반복문 끝

--- practice3.php
+++ practice3.php
@@ -10,6 +10,45 @@
  </head>
  <body>
 	<h1>반복문 연습!</h1>
-    dmdmdmdm
+    
+    <h3>for문</h3>
+    <p>
+        <?php
+            $star = "*";
+
+            for($i = 0; $i < 5; $i++) {
+                echo $star;
+                echo "<br>";
+                $star .= "*";
+            }
+        ?>
+    </p>
+
+    <h3>while문</h3>
+    <p>
+        <?php
+            $star = "*";
+            $i = 0;
+
+            while ($i < 5) {
+                echo $star;
+                echo "<br>";
+                $star .= "*";
+                $i++;
+            }
+        ?>
+    </p>
+
+    <h3>foreach문</h3>
+    <p>
+        <?php
+            $arr = array(1, 2, 3, 4, 5);
+            foreach ($arr as $val) {
+                $val = $val * 2;
+                echo $val;
+                echo "<br>";
+            }
+        ?>
+    </p>
  </body>
 </html>
